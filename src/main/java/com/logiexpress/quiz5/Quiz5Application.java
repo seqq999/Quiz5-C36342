97923dd3d5a7ff53e2641cace74258189a6e277a
seqq999 <sebas182005@gmail.com>
2026-09-16 15:45:49 -0600

    despach: commit inicial

---
+++ src/main/java/com/logiexpress/quiz5/Quiz5Application.java
@@ -0,0 +1,13 @@
+package com.logiexpress.quiz5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz5Application.class, args);
+	}
+
+}
