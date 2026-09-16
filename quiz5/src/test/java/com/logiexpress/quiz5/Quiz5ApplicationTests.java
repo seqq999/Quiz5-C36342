552fc46e1bb93354cba19b5a95cbc2e8ea0368fa
seqq999 <sebas182005@gmail.com>
2026-09-16 15:42:07 -0600

    despach: commit inicial

---
+++ quiz5/src/test/java/com/logiexpress/quiz5/Quiz5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiexpress.quiz5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
